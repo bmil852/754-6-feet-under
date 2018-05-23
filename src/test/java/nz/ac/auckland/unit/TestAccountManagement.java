e7d79effe58d80f3606a568b2533086c590aae97
sindhusingamneni <sindhusingamneni@gmail.com>
2018-05-23 20:27:29 +1200

    Add test for admin invalid sign-in for Story 1

--- src/test/java/nz/ac/auckland/unit/TestAccountManagement.java
+++ src/test/java/nz/ac/auckland/unit/TestAccountManagement.java
@@ -55,4 +55,19 @@ public class TestAccountManagement {
         //Then
         fail("User authentication failed - invalid username or password provided for user");
     }
+
+    @Test(expected=RuntimeException.class)
+    public void when_administrator_wants_to_sign_in_with_invalid_credentials() {
+        //Given
+        Client client = new Client("Admin1", "AdminPassword1");
+        _login = new Login();
+        _login.register(client, Role.ADMINISTRATOR);
+
+        //When
+        _login.signIn("WrongAdmin1", "WrongAdminPassword1", Role.ADMINISTRATOR);
+        _login.checkClientSignedIn(client, Role.ADMINISTRATOR);
+
+        //Then
+        fail("Administrator authentication failed - invalid username or password provided for administrator");
+    }
 }
