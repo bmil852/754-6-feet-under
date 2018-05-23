e9592c593f7ca184caeaa593f26dbf7bfc9e2643
Shibani T <stim599@aucklanduni.ac.nz>
2018-05-23 15:22:00 +1200

    Refactor implementation for Story 11

--- src/main/java/nz/ac/auckland/Category.java
+++ src/main/java/nz/ac/auckland/Category.java
@@ -1,6 +1,5 @@
 package nz.ac.auckland;
 
-
 import java.util.ArrayList;
 import java.util.List;
 
--- src/test/java/nz/ac/auckland/unit/TestBusinessValidation.java
+++ src/test/java/nz/ac/auckland/unit/TestBusinessValidation.java
@@ -2,25 +2,21 @@ package nz.ac.auckland.unit;
 
 import nz.ac.auckland.Category;
 import nz.ac.auckland.Document;
+import org.junit.Before;
 import org.junit.Test;
-
 import java.util.*;
-
 import static org.hamcrest.CoreMatchers.equalTo;
-
 import static org.junit.Assert.assertThat;
 
 public class TestBusinessValidation {
     Category c1, c2, c3;
+    Set<Category> categories = new HashSet<>();
 
-    @Test
-    public void calculate_popularity_of_a_category(){
-        //Given
-        Set<Category> categories = new HashSet<>();
+    @Before
+    public void setUp(){
         c1 = new Category("C1");
         c2 = new Category("C2");
         c3 = new Category("C3");
-
         for (int i = 0; i < 100; i++) {
             if (i < 50) {
                 Document d1 = new Document("Mock text for a document of category 1", c1);
@@ -36,12 +32,17 @@ public class TestBusinessValidation {
         categories.add(c1);
         categories.add(c2);
         categories.add(c3);
+    }
 
-        //When
+    @Test
+    public void calculate_popularity_of_a_category(){
+        //Given
+        Set<Category> _categories = categories;
         Map<Category,Double> documentCounts = new HashMap();
         double total = 0.0;
 
-        for(Category c : categories){
+        //When
+        for(Category c : _categories){
             documentCounts.put(c,(double) c.getDocumentCount());
             total += (double) c.getDocumentCount();
         }
