444f6a901af7d205aebfb4ea34d929c824d2a6d6
Shibani T <stim599@aucklanduni.ac.nz>
2018-05-24 18:32:09 +1200

    Add implementation for Story 7 to define step definitions

---
+++ src/test/java/nz/ac/auckland/acceptance/MarketComprehensionStepDefinitions.java
@@ -0,0 +1,23 @@
+package nz.ac.auckland.acceptance;
+
+
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class MarketComprehensionStepDefinitions {
+    @Given("^user has a set of weighted keywords to search for$")
+    public void User_has_a_set_of_weighted_keywords_to_search_for() {
+
+    }
+
+    @When("^user performs a search$")
+    public void User_performs_a_search() {
+
+    }
+
+    @Then("^a non-empty set of documents are returned$")
+    public void non_empty_set_of_documents_are_returned() {
+
+    }
+}
