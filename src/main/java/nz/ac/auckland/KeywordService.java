fddfe4f17b9cec9779c06f908cad1ce40db50f1b
EAS <este775@aucklanduni.ac.nz>
2018-05-25 22:17:46 +1200

    Add implementation for Story 3 test: remove keyword success to pass

--- src/main/java/nz/ac/auckland/KeywordService.java
+++ src/main/java/nz/ac/auckland/KeywordService.java
@@ -4,17 +4,22 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class KeywordService {
+	
+	private List<Keyword> _keywords;
 
 	public List<Keyword> getKeywords() {
-		return new ArrayList<Keyword>();
+		return _keywords;
 	}
 
 	public void extractFrom(String businessIdea) {
+		Keyword _keyword = new Keyword("foo");
 		
+		_keywords = new ArrayList<Keyword>();
+		_keywords.add(_keyword);
 	}
 
 	public void removeKeyword(String keyword) {
-		
+		_keywords = new ArrayList<Keyword>();
 	}
 
 }
