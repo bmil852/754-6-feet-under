e1b609a706cc9f3b9c2157c350b3f56858234fa9
EAS <este775@aucklanduni.ac.nz>
2018-05-26 18:31:24 +1200

    Add implementation for Story 5 test: invalid keyword weight update to compile

--- src/test/java/nz/ac/auckland/unit/TestKeywordExtraction.java
+++ src/test/java/nz/ac/auckland/unit/TestKeywordExtraction.java
@@ -3,6 +3,7 @@ package nz.ac.auckland.unit;
 import nz.ac.auckland.AlreadyExistingKeywordException;
 import nz.ac.auckland.EmptyInputException;
 import nz.ac.auckland.InsufficientInformationException;
+import nz.ac.auckland.InvalidKeywordWeightException;
 import nz.ac.auckland.Keyword;
 import nz.ac.auckland.KeywordExtractor;
 import nz.ac.auckland.KeywordService;
