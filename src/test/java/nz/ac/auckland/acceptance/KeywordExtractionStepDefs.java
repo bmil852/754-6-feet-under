7e65806017b85faf7ce04219fb0197f311a3c389
EAS <este775@aucklanduni.ac.nz>
2018-05-26 16:01:24 +1200

    Add BDD test/stepdefs for Story 5: change keyword weight success
    Doesn't compile.

---
+++ src/test/java/nz/ac/auckland/acceptance/KeywordExtractionStepDefs.java
@@ -0,0 +1,43 @@
+package nz.ac.auckland.acceptance;
+
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class KeywordExtractionStepDefs {
+
+	private KeywordService _keywordService;
+
+	@Given("^a User already has a number of keywords returned$")
+	public void a_User_already_has_a_number_of_keywords_returned() throws Exception {
+		List<Keyword> extractedKeywords = new ArrayList<Keyword>();
+		extractedKeywords.add(new Keyword("Dog"));
+		extractedKeywords.add(new Keyword("Walking"));
+		extractedKeywords.add(new Keyword("Ponsonby"));
+
+		KeywordExtractor _keywordExtractor = mock(KeywordExtractor.class);
+		when(_keywordExtractor.extractFrom("A dog walking service in Ponsonby")).thenReturn(extractedKeywords);
+		this._keywordService = new KeywordService(_keywordExtractor);
+
+		this._keywordService.extractFrom("A dog walking service in Ponsonby");
+	}
+
+	@When("^the User requests to change the weight of keyword \"(.*)\" to (\\d+)$")
+	public void the_User_requests_to_change_the_weight_of_keyword_to(String word, int newWeight) throws Exception {
+		this._keywordService.updateWeight(word, newWeight);
+	}
+
+	@Then("^the weight of keyword \"(.*)\" is updated to (\\d+)$")
+	public void the_weight_of_keyword_is_updated_to(String word, int newWeight) throws Exception {
+		List<Keyword> extractedKeywords = this._keywordService.getKeywords();
+		for (int i = 0; i < extractedKeywords.size(); i++) {
+			Keyword keyword = extractedKeywords.get(i);
+			if (keyword.word.equals(word)) {
+				assertEquals(keyword.getWeight(), newWeight);
+			}
+		}
+	}
+}
